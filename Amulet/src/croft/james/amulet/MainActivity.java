d09a3aee87ba420d01cc673462b88bf28d7b5a0c
James Croft <me@jamescroft.co.uk>
2014-03-26 10:59:46 +0000

    Implemented Navigation Drawer
    Fully implemented a simple navigation drawer (requires fragments)
    
    Added support for multiple screen resolutions.

--- Amulet/src/croft/james/amulet/MainActivity.java
+++ Amulet/src/croft/james/amulet/MainActivity.java
@@ -7,13 +7,13 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
 	private DrawerLayout _drawerLayout;
 	private ListView _drawerList;
 	private ActionBarDrawerToggle _drawerToggle;
@@ -28,7 +28,6 @@ public class MainActivity extends Activity {
         setupDrawer(savedInstanceState);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -36,19 +35,68 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+    	boolean drawerOpen = _drawerLayout.isDrawerOpen(_drawerList);
+    	menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+    	return super.onPrepareOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	if(_drawerToggle.onOptionsItemSelected(item)){
+    		return true;
+    	}
+    	
+    	switch(item.getItemId()){
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
+    }
+    
+	@Override
+	public void setTitle(CharSequence title){
+		_title = title;
+		getActionBar().setTitle(_title);
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState){
+		super.onPostCreate(savedInstanceState);
+		_drawerToggle.syncState();
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig){
+		super.onConfigurationChanged(newConfig);
+		_drawerToggle.onConfigurationChanged(newConfig);
+	}
+    
+    /**
+     * Sets up the Android Navigation Drawer for the application.
+     * This method will gather menu items from a string array and
+     * will associate them with the drawer. When this is done it
+     * assigns the drawer to the ActionBar of the current layout with
+     * event handlers for opening and closing the drawer.
+     * 
+     * @param savedInstanceState the bundle that is passed through onCreate override method
+     */
     private void setupDrawer(Bundle savedInstanceState){
         _title = _drawerTitle = getTitle();
     	_menuItems = getResources().getStringArray(R.array.menu_items);
     	_drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
     	_drawerList = (ListView)findViewById(R.id.left_drawer);
     	
-    	_drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+    	// Create a shadow to overlay main content
+    	_drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START); 
+    	// Setup the drawer's items with the menu items gathered from the string array
     	_drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, _menuItems));
     	_drawerList.setOnItemClickListener(new DrawerItemClickListener());
     	
     	getActionBar().setDisplayHomeAsUpEnabled(true);
     	getActionBar().setHomeButtonEnabled(true);
     	
+    	// Ties the ActionBar and the Navigation Drawer
     	_drawerToggle = new ActionBarDrawerToggle(
     			this,
     			_drawerLayout,
@@ -89,21 +137,5 @@ public class MainActivity extends Activity {
 		_drawerLayout.closeDrawer(_drawerList);
 	}
 	
-	@Override
-	public void setTitle(CharSequence title){
-		_title = title;
-		getActionBar().setTitle(_title);
-	}
-	
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState){
-		super.onPostCreate(savedInstanceState);
-		_drawerToggle.syncState();
-	}
-	
-	@Override
-	public void onConfigurationChanged(Configuration newConfig){
-		super.onConfigurationChanged(newConfig);
-		_drawerToggle.onConfigurationChanged(newConfig);
-	}
+
 }
