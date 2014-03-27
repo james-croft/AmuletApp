2d857d2cf821f61b80fc2a0ec1ab760de9dbc657
James Croft <me@jamescroft.co.uk>
2014-03-27 11:01:29 +0000

    Created new Launcher logo and improved login
    Improved the login functionality including hiding/showing the login or
    register forms.
    
    Also created and added a logo for the application

--- Amulet/gen/croft/james/amulet/R.java
+++ Amulet/gen/croft/james/amulet/R.java
@@ -30,16 +30,24 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f090008;
-        public static final int action_settings=0x7f090009;
-        public static final int content_frame=0x7f090006;
-        public static final int drawer_layout=0x7f090005;
-        public static final int left_drawer=0x7f090007;
-        public static final int login_form=0x7f090002;
-        public static final int login_status=0x7f090000;
-        public static final int login_status_message=0x7f090001;
-        public static final int register_button=0x7f090004;
-        public static final int sign_in_button=0x7f090003;
+        public static final int action_forgot_password=0x7f090010;
+        public static final int action_settings=0x7f090011;
+        public static final int content_frame=0x7f09000e;
+        public static final int drawer_layout=0x7f09000d;
+        public static final int email=0x7f09000a;
+        public static final int first_name=0x7f090008;
+        public static final int left_drawer=0x7f09000f;
+        public static final int login_form=0x7f090003;
+        public static final int login_selection=0x7f090000;
+        public static final int login_user_button=0x7f090006;
+        public static final int password=0x7f090005;
+        public static final int register_button=0x7f090002;
+        public static final int register_form=0x7f090007;
+        public static final int register_password=0x7f09000b;
+        public static final int register_user_button=0x7f09000c;
+        public static final int sign_in_button=0x7f090001;
+        public static final int surname=0x7f090009;
+        public static final int username=0x7f090004;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -51,39 +59,41 @@ public final class R {
         public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f05000c;
-        public static final int action_register=0x7f05000a;
+        public static final int action_forgot_password=0x7f05000e;
+        public static final int action_register=0x7f05000c;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in=0x7f050009;
-        public static final int action_sign_in_short=0x7f05000b;
+        public static final int action_sign_in=0x7f05000b;
+        public static final int action_sign_in_short=0x7f05000d;
         public static final int app_name=0x7f050000;
         public static final int app_not_available=0x7f050005;
-        public static final int change_password=0x7f050015;
+        public static final int change_password=0x7f050017;
         public static final int drawer_close=0x7f050004;
         public static final int drawer_open=0x7f050003;
-        public static final int drink=0x7f05001a;
-        public static final int drink_diary=0x7f050018;
-        public static final int error_field_required=0x7f050011;
-        public static final int error_incorrect_password=0x7f050010;
-        public static final int error_invalid_email=0x7f05000e;
-        public static final int error_invalid_password=0x7f05000f;
-        public static final int full_details=0x7f050016;
+        public static final int drink=0x7f05001c;
+        public static final int drink_diary=0x7f05001a;
+        public static final int error_field_required=0x7f050013;
+        public static final int error_incorrect_password=0x7f050012;
+        public static final int error_invalid_email=0x7f050010;
+        public static final int error_invalid_password=0x7f050011;
+        public static final int full_details=0x7f050018;
         public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f05000d;
+        public static final int login_progress_signing_in=0x7f05000f;
         /**  Strings related to login 
          */
         public static final int prompt_email=0x7f050007;
-        public static final int prompt_password=0x7f050008;
+        public static final int prompt_first_name=0x7f050008;
+        public static final int prompt_password=0x7f05000a;
+        public static final int prompt_surname=0x7f050009;
         /**  GET requests 
          */
-        public static final int register_user=0x7f050013;
+        public static final int register_user=0x7f050015;
         /**  POST requests 
          */
-        public static final int task=0x7f050019;
-        public static final int task_history=0x7f050017;
+        public static final int task=0x7f05001b;
+        public static final int task_history=0x7f050019;
         public static final int title_activity_login=0x7f050006;
-        public static final int unregister_user=0x7f050014;
-        public static final int web_service_url=0x7f050012;
+        public static final int unregister_user=0x7f050016;
+        public static final int web_service_url=0x7f050014;
     }
     public static final class style {
         /** 
--- Amulet/src/croft/james/amulet/LoginActivity.java
+++ Amulet/src/croft/james/amulet/LoginActivity.java
@@ -1,21 +1,49 @@
 package croft.james.amulet;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity implements OnRetrieveDataCompleted {	
+	View _currentView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+			    View loginForm = findViewById(R.id.login_form);
+			    _currentView = loginForm;
+				showForm(loginForm, true);
+			}
+		});
+		
+		findViewById(R.id.register_button).setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				View registerForm = findViewById(R.id.register_form);
+				_currentView = registerForm;
+				showForm(registerForm, true);
+			}
+		});
+	}
+	
+	@Override 
+	public void onBackPressed(){
+		if(_currentView != null){
+			showForm(_currentView, false);
+			_currentView = null;
+		} else {
+			super.onBackPressed();
+		}
 	}
 
 	@Override
@@ -24,18 +52,28 @@ public class LoginActivity extends Activity implements OnRetrieveDataCompleted {
 		startActivity(intent);
 	}
 	
-	public void onBtnClicked(View v){
-		if(v.getId() == R.id.sign_in_button)
-			loginUser();
+	public void showForm(View v, final boolean show){
+		View selectionOptions = findViewById(R.id.login_selection);
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+			int shortAnimTime = getResources().getInteger(
+					android.R.integer.config_shortAnimTime);
+
+			v.setVisibility(View.VISIBLE);
+			v.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0);
+
+			selectionOptions.setVisibility(View.VISIBLE);
+			selectionOptions.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1);
+		} else {
+			// The ViewPropertyAnimator APIs are not available, so simply show and hide the relevant UI components.
+			v.setVisibility(show ? View.VISIBLE : View.GONE);
+			selectionOptions.setVisibility(show ? View.GONE : View.VISIBLE);
+		}
 	}
 	
 	private void loginUser(){
 		Toast.makeText(this, getString(R.string.login_progress_signing_in), Toast.LENGTH_LONG).show();
 		RetrieveHTTPDataAsync loginData = new RetrieveHTTPDataAsync(this);
 		loginData.execute(getString(R.string.web_service_url) + getString(R.string.register_user));
-	}
-	
-	private void registerUser(){
-		
 	}
 }
