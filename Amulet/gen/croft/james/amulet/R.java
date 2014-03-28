de11910f4dea5d8b35cefaeef348ff92f6649613
James Croft <me@jamescroft.co.uk>
2014-03-28 08:37:33 +0000

    Fixed simple login functionality
    Login functionality now connects with the web service and works
    flawlessly.
    
    Login forms now require validation and parsing the returned JSON
    objects.

--- Amulet/gen/croft/james/amulet/R.java
+++ Amulet/gen/croft/james/amulet/R.java
@@ -66,16 +66,16 @@ public final class R {
         public static final int action_sign_in_short=0x7f05000d;
         public static final int app_name=0x7f050000;
         public static final int app_not_available=0x7f050005;
-        public static final int change_password=0x7f050017;
+        public static final int change_password=0x7f050018;
         public static final int drawer_close=0x7f050004;
         public static final int drawer_open=0x7f050003;
-        public static final int drink=0x7f05001c;
-        public static final int drink_diary=0x7f05001a;
-        public static final int error_field_required=0x7f050013;
-        public static final int error_incorrect_password=0x7f050012;
-        public static final int error_invalid_email=0x7f050010;
-        public static final int error_invalid_password=0x7f050011;
-        public static final int full_details=0x7f050018;
+        public static final int drink=0x7f05001d;
+        public static final int drink_diary=0x7f05001b;
+        public static final int error_field_required=0x7f050014;
+        public static final int error_incorrect_password=0x7f050013;
+        public static final int error_invalid_email=0x7f050011;
+        public static final int error_invalid_password=0x7f050012;
+        public static final int full_details=0x7f050019;
         public static final int hello_world=0x7f050002;
         public static final int login_progress_signing_in=0x7f05000f;
         /**  Strings related to login 
@@ -84,16 +84,17 @@ public final class R {
         public static final int prompt_first_name=0x7f050008;
         public static final int prompt_password=0x7f05000a;
         public static final int prompt_surname=0x7f050009;
+        public static final int register_progress_registering=0x7f050010;
         /**  GET requests 
          */
-        public static final int register_user=0x7f050015;
+        public static final int register_user=0x7f050016;
         /**  POST requests 
          */
-        public static final int task=0x7f05001b;
-        public static final int task_history=0x7f050019;
+        public static final int task=0x7f05001c;
+        public static final int task_history=0x7f05001a;
         public static final int title_activity_login=0x7f050006;
-        public static final int unregister_user=0x7f050016;
-        public static final int web_service_url=0x7f050014;
+        public static final int unregister_user=0x7f050017;
+        public static final int web_service_url=0x7f050015;
     }
     public static final class style {
         /** 
--- Amulet/src/croft/james/amulet/LoginActivity.java
+++ Amulet/src/croft/james/amulet/LoginActivity.java
@@ -1,12 +1,11 @@
 package croft.james.amulet;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity implements OnRetrieveDataCompleted {	
@@ -32,6 +31,20 @@ public class LoginActivity extends Activity implements OnRetrieveDataCompleted {
 				View registerForm = findViewById(R.id.register_form);
 				_currentView = registerForm;
 				showForm(registerForm, true);
+			}
+		});
+		
+		findViewById(R.id.login_user_button).setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				loginUser();
+			}
+		});
+		
+		findViewById(R.id.register_user_button).setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				registerUser();
 			}
 		});
 	}
@@ -73,7 +86,23 @@ public class LoginActivity extends Activity implements OnRetrieveDataCompleted {
 	
 	private void loginUser(){
 		Toast.makeText(this, getString(R.string.login_progress_signing_in), Toast.LENGTH_LONG).show();
+		
+		String username = ((EditText)findViewById(R.id.username)).getText().toString();
+		String password = ((EditText)findViewById(R.id.password)).getText().toString();
+		
 		RetrieveHTTPDataAsync loginData = new RetrieveHTTPDataAsync(this);
-		loginData.execute(getString(R.string.web_service_url) + getString(R.string.register_user));
+		loginData.execute(getString(R.string.web_service_url) + getString(R.string.full_details) + String.format("username=%1$s&password=%2$s", username, password));
+	}
+	
+	private void registerUser(){
+		Toast.makeText(this, getString(R.string.register_progress_registering), Toast.LENGTH_LONG).show();
+		
+		String firstName = ((EditText)findViewById(R.id.first_name)).getText().toString();
+		String surname = ((EditText)findViewById(R.id.surname)).getText().toString();
+		String username = ((EditText)findViewById(R.id.email)).getText().toString();
+		String password = ((EditText)findViewById(R.id.register_password)).getText().toString();
+		
+		RetrieveHTTPDataAsync loginData = new RetrieveHTTPDataAsync(this);
+		loginData.execute(getString(R.string.web_service_url) + getString(R.string.register_user) + String.format("firstname=%1$s&Surname=%2$s&username=%3$s&password=%4$s", firstName, surname, username, password));
 	}
 }
